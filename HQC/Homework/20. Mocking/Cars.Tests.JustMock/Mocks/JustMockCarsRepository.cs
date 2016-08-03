0ec4d2838601cdc132e730052f97f73d0d9d2284
Andriyan Krastev <andrian_29@abv.bg>
2016-08-04 00:37:37 +0300

    Add Mocking homework.

--- HQC/Homework/20. Mocking/Cars.Tests.JustMock/Mocks/JustMockCarsRepository.cs
+++ HQC/Homework/20. Mocking/Cars.Tests.JustMock/Mocks/JustMockCarsRepository.cs
@@ -12,6 +12,7 @@
         protected override void ArrangeCarsRepositoryMock()
         {
             this.CarsData = Mock.Create<ICarsRepository>();
+
             Mock.Arrange(() => this.CarsData.Add(Arg.IsAny<Car>())).DoNothing();
             Mock.Arrange(() => this.CarsData.All()).Returns(this.FakeCarCollection);
             Mock.Arrange(() => this.CarsData.Search(Arg.AnyString)).Returns(this.FakeCarCollection.Where(c => c.Make == "BMW").ToList());
