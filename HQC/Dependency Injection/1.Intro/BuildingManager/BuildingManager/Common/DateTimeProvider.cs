51916262b6795b55ff6c1f2ce6ad93882494087e
chunk1ty <andriyan.krastev@gmail.com>
2020-01-15 23:16:46 +0200

    Added AmbientContext

---
+++ HQC/Dependency Injection/1.Intro/BuildingManager/BuildingManager/Common/DateTimeProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BuildingManager.Common
+{
+    public static class DateTimeProvider
+    {
+        private static ITimeService _timeService = new TimeService();
+
+        public static void Initialize(ITimeService dateTimeService)
+        {
+            _timeService = dateTimeService;
+        }
+
+        public static ITimeService Instance => _timeService;
+    }
+
+    public interface ITimeService
+    {
+        DateTime UtcNow { get; }
+    }
+
+    public class TimeService : ITimeService
+    {
+        public DateTime UtcNow => DateTime.UtcNow;
+    }
+}
---
+++ HQC/Dependency Injection/1.Intro/BuildingManager/BuildingManager/Common/DateTimeProviderAmbientContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BuildingManager.Common
+{
+    public abstract class DateTimeProviderAmbientContext 
+    {
+        private static DateTimeProviderAmbientContext _instance;
+
+        static DateTimeProviderAmbientContext()
+        {
+            Instance = new DefaultDateTimeProviderAmbientContext();
+        }
+
+        public abstract DateTime UtcNow { get; }
+
+        public static DateTimeProviderAmbientContext Instance
+        {
+            get => _instance;
+            set => _instance = value ?? throw new ArgumentNullException(nameof(value));
+        }
+    }
+
+    public class DefaultDateTimeProviderAmbientContext : DateTimeProviderAmbientContext
+    {
+        public override DateTime UtcNow => DateTime.UtcNow;
+    }
+}
