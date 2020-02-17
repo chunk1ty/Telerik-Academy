7b43b9c629df75bc18b9eb933de6c2c425e0d79e
andriyan-krastev <andriyan.krastev@fourth.com>
2020-02-17 11:13:40 +0200

    refactored Facade demo

--- HQC/Demo/18. Design Patterns/Structural/Facade/Program.cs
+++ HQC/Demo/18. Design Patterns/Structural/Facade/Program.cs
@@ -2,15 +2,55 @@
 {
     public static class Program
     {
+        /* Client */
         public static void Main()
         {
-            var homeTheaterPro = HomeTheaterPro.CreateInstance();
-            homeTheaterPro.InitHomeSystem();
-            homeTheaterPro.DisplayAvailableMedia();
-            homeTheaterPro.Next();
-            homeTheaterPro.Next();
-            homeTheaterPro.Previous();
-            homeTheaterPro.Stop();
+            ComputerFacade computer = new ComputerFacade();
+            computer.start();
+        }
+    }
+
+    /* Facade */
+    class ComputerFacade
+    {
+        private CPU processor;
+        private Memory ram;
+        private HardDrive hd;
+
+        public ComputerFacade()
+        {
+            this.processor = new CPU();
+            this.ram = new Memory();
+            this.hd = new HardDrive();
+        }
+
+        public void start()
+        {
+            //processor.freeze();
+            //ram.load(BOOT_ADDRESS, hd.read(BOOT_SECTOR, SECTOR_SIZE));
+            //processor.jump(BOOT_ADDRESS);
+            //processor.execute();
+        }
+    }
+
+    /* Complex parts */
+    class CPU
+    {
+        public void freeze() { }
+        public void jump(long position) { }
+        public void execute() { }
+    }
+
+    class Memory
+    {
+        public void load(long position, byte[] data) { }
+    }
+
+    class HardDrive
+    {
+        public byte[] read(long lba, int size)
+        {
+            return new byte[1];
         }
     }
 }
